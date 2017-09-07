b7a90941099ea745337999d2d99ee4613c65605c
kimbroughd <31328710+kimbroughd@users.noreply.github.com>
2017-09-07 16:14:53 -0400

    Update Mocha.java

--- src/headfirst/designpatterns/decorator/starbuzz/Mocha.java
+++ src/headfirst/designpatterns/decorator/starbuzz/Mocha.java
@@ -1,17 +1,16 @@
 package headfirst.designpatterns.decorator.starbuzz;
 
 public class Mocha extends CondimentDecorator {
-	Beverage beverage;
  
 	public Mocha(Beverage beverage) {
-		this.beverage = beverage;
+		super.CondimentDecorator(beverage);
 	}
  
 	public String getDescription() {
-		return beverage.getDescription() + ", Mocha";
+		return wrappedBeverage.getDescription() + ", Mocha";
 	}
  
 	public double cost() {
-		return .20 + beverage.cost();
+		return .20 + wrappedBeverage.cost();
 	}
 }
